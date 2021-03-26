0a1d5e77242b08647174933ddc4261b7dc02d880
Ronak99 <punase.ronak99@gmail.com>
2021-03-26 16:33:49 +0530

    Deleted profile

--- index.js
+++ index.js
@@ -56,10 +56,10 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello World! at port : ${PORT}`)
 })
 
 app.listen(port, () => {
